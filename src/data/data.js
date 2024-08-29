8a695e6ef25a3aabd127b3c6ad3259ae6b6cd2a7
Ibukun Alao <ibkalao@yahoo.com>
2024-08-29 09:02:03 -0600

    work data update

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ export const data = [
     img: "https://i.postimg.cc/26s1YMLr/Screenshot-2024-01-20-013837.png",
     title: "Campground Review website",
     desc: "",
-    link: "https://tender-jade-rabbit.cyclic.app/",
+    link: "https://yelpcamp-r1x5.onrender.com/",
     git: "",
   },
   {
@@ -12,6 +12,13 @@ export const data = [
     img: "https://i.postimg.cc/MGLdwPdY/Screenshot-2024-01-21-000746.png",
     title: "Gadget e-commerce website",
     link: "https://muubaraq.github.io/kodetech-test/pages/shop.html",
+    git: "",
+  },
+  {
+    id: 3,
+    img: "https://i.postimg.cc/nr2mwHG5/Screenshot-2024-08-29-085618.png",
+    title: "Mountain of Mercy",
+    link: "https://mt-of-mercy.netlify.app/",
     git: "",
   },
 ];
